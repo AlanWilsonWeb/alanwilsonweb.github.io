f8e6b8b46f62ddd3b194a50cda67bfed6a5d91dd
Unknown <nannerfox@me.com>
2018-04-28 22:18:18 -0500

    Style Changes
    Changed colors, added smooth scrolling to links, added containers to each "item"

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function moveToJum() {
-    window.scrollTo(0, 0);
-};
+// bg-primary codes:
+// #007bff
+// rgb(0, 123, 255)
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
