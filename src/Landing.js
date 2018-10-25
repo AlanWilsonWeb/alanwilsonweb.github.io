35ca0f1b11b748a0d3564caba8dff8d96e704581
Unknown <nannerfox@me.com>
2018-10-25 17:21:33 -0500

    Landing page icons

--- src/Landing.js
+++ src/Landing.js
@@ -14,27 +14,34 @@ class Landing extends React.Component {
         <Fade in={this.state.fadein}>
       <Jumbotron className="text-center bg-primary text-white">
         <h1>Hi, I'm Alan</h1>
-        <h5>I make Full Stack Web pages</h5>
+        <h5>I Write Code!</h5>
       </Jumbotron>
       <Jumbotron className="bg-dark text-center">
         <h3>With Emphasis and Proficiency In:</h3>
-      {/* <i>I'm seeking to secure an entry-level Programming and Development position that will utilize, challenge, and expand my education, experience, and overall passion for Development, while working to enhance my employers’ operations, and provide reinforcement for those working alongside of me."</i> */}
-      <Row>
-          <Col md="2"><a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"><i className="fab fab fa-html5 fa-5x blueicon"></i><h6>HTML</h6></a></Col>
-          <Col md="2"><a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><i className="fab fa-js-square fa-5x blueicon"></i><h6>JavaScript</h6></a></Col>
-          <Col md="2"><a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><i className="fab fa-css3 fa-5x blueicon"></i><h6>CSS</h6></a></Col>
-          <Col md="2"><a rel="noopener noreferrer" href="https://reactjs.org/" target="_blank"><i className="fab fa-react fa-5x blueicon"></i><h6>ReactJS</h6></a></Col>
-          <Col md="2"><a rel="noopener noreferrer" href="https://nodejs.org/en/" target="_blank"><i className="fab fa-node fa-5x blueicon"></i><h6>NodeJS</h6></a></Col>
-          <Col md="2"><a rel="noopener noreferrer" href="https://git-scm.com/" target="_blank"><i className="fab fa-git-square fa-5x blueicon"></i><h6>Git/Version Control</h6></a></Col>
-      </Row>
-      <Row>
-          <Col md="2"><i className="fas fa-database fa-5x blueicon"></i><h6>SQL/noSQL Databases</h6></Col>
-          <Col md="2"><a rel="noopener noreferrer" href="https://angularjs.org/" target="_blank"><i className="fab fa-angular fa-5x blueicon"></i><h6>AngularJS</h6></a></Col>
-          <Col md="2"><i className="fab fa-windows fa-5x blueicon"></i><h6>Desktop Applications</h6></Col>
-          <Col md="2"><a rel="noopener noreferrer" href="https://wordpress.com/" target="_blank"><i className="fab fa-wordpress fa-5x blueicon"></i><h6>Wordpress</h6></a></Col>
-          <Col md="2"><i className="fab fa-aws fa-5x blueicon"></i><h6>Deployment Options</h6></Col>
-          <Col md="2"><i className="fas fa-mobile-alt fa-5x blueicon"></i><h6>Mobile & Responsive Design</h6></Col>
-      </Row>
+      <a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"><div className="landingIcons"><i className="fab fab fa-html5 fa-2x"></i> HTML</div></a>
+      <br />
+      <a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><div className="landingIcons"><i className="fab fa-js-square fa-2x"></i> JavaScript</div></a>
+      <br />
+      <a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><div className="landingIcons"><i className="fab fa-css3 fa-2x"></i> CSS</div></a>
+      <br />
+      <a rel="noopener noreferrer" href="https://reactjs.org/" target="_blank"><div className="landingIcons"><i className="fab fa-react fa-2x"></i> ReactJS</div></a>
+      <br />
+      <a rel="noopener noreferrer" href="https://nodejs.org/en/" target="_blank"><div className="landingIcons"><i className="fab fa-node fa-2x"></i> NodeJS</div></a>
+      <br />
+      <a rel="noopener noreferrer" href="https://git-scm.com/" target="_blank"><div className="landingIcons"><i className="fab fa-git-square fa-2x"></i> Git/Version Control</div></a>
+      <br />
+      <div className="landingIcons"><i className="fas fa-database fa-2x"></i> SQL/noSQL Databases</div>
+      <br />
+      <a rel="noopener noreferrer" href="https://angularjs.org/" target="_blank"><div className="landingIcons"><i className="fab fa-angular fa-2x"></i> AngularJS</div></a>
+      <br />
+      <div className="landingIcons"><i className="fab fa-windows fa-2x"></i> Desktop Applications</div>
+      <br />
+      <a rel="noopener noreferrer" href="https://wordpress.com/" target="_blank"><div className="landingIcons"><i className="fab fa-wordpress fa-2x"></i> Wordpress</div></a>
+      <br />
+      <div className="landingIcons"><i className="fab fa-aws fa-2x"></i> Deployment Options</div>
+      <br />
+      <div className="landingIcons"><i className="fas fa-mobile-alt fa-2x"></i> Mobile & Responsive Design</div>
+      <br />
       </Jumbotron>
     </Fade>
   </div>
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -32,7 +32,7 @@ class Portfolio extends React.Component {
           <h2><u>MovieFive</u></h2>
           A Full-Stack project site where a User can create a list of their Top-5 favorite movies, and share it in a variety of methods (email, link, social media, etc). The site was built in React, utilizing a NodeJS and ExpressJS back-end server, MongoDB/mLab Database, Heroku Live Deployment, and Bootstrap 3 with CSS for styling. This group project earned me the final certification as a Developer through the Full-Stack Web curriculum at Woz-U.
           <br />
-          <Button href="https://moviefive.herokuapp.com" target="_blank" color="primary"><i class="far fa-eye"></i> Live Site</Button> <Button href="https://github.com/5-28-a/group-project" target="_blank" color="primary"><i className="fas fa-code"></i> View Code</Button>
+          <Button className="buttonz" href="https://moviefive.herokuapp.com" target="_blank" color="primary"><i class="far fa-eye"></i> Live Site</Button> <Button className="buttonz" href="https://github.com/5-28-a/group-project" target="_blank" color="primary"><i className="fas fa-code"></i> View Code</Button>
         </Col>
       </Row>
       <br />
@@ -42,7 +42,7 @@ class Portfolio extends React.Component {
           <h2><u>React-HashRouter</u></h2>
           A lightweight routing alternative for the ReactJS framework. This routing solution controls navigation through the use of #hashes and component state. This solution is very useful in an environment where /slash/ routing is proxied and the React Application needs to be "single page." This also allows add-on libraries, like a Redux Store, to not lose their state during a page transition which re-loads the entire application.
           <br />
-          <Button href="https://nannerfox.herokuapp.com" target="_blank" color="primary"><i class="far fa-eye"></i> Live Site</Button> <Button href="https://github.com/NannerFox/React-HashRouter" target="_blank" color="primary"><i className="fas fa-code"></i> View Code</Button> <Button href="https://nannerfox.wordpress.com/2018/07/16/the-scenic-route/" target="_blank" color="primary"><i className="fab fa-wordpress-simple"></i> View Blog Post</Button>
+          <Button className="buttonz" href="https://nannerfox.herokuapp.com" target="_blank" color="primary"><i class="far fa-eye"></i> Live Site</Button> <Button className="buttonz" href="https://github.com/NannerFox/React-HashRouter" target="_blank" color="primary"><i className="fas fa-code"></i> View Code</Button> <Button className="buttonz" href="https://nannerfox.wordpress.com/2018/07/16/the-scenic-route/" target="_blank" color="primary"><i className="fab fa-wordpress-simple"></i> View Blog Post</Button>
         </Col>
       </Row>
       <br />
@@ -52,7 +52,7 @@ class Portfolio extends React.Component {
           <h2><u>JavaScript Quiz</u></h2>
           A fun, small project, which quizzes the user on some various knowledge related to core JavaScript (no extenal libraries or frameworks).
           <br />
-          <Button href="https://awjavascriptquiz.herokuapp.com/" target="_blank" color="primary"><i class="far fa-eye"></i> Live Site</Button> <Button href="https://github.com/NannerFox/JavaScript-Quiz" target="_blank" color="primary"><i className="fas fa-code"></i> View Code</Button>
+          <Button className="buttonz" href="https://awjavascriptquiz.herokuapp.com/" target="_blank" color="primary"><i class="far fa-eye"></i> Live Site</Button> <Button className="buttonz" href="https://github.com/NannerFox/JavaScript-Quiz" target="_blank" color="primary"><i className="fas fa-code"></i> View Code</Button>
         </Col>
       </Row>
       <br />
@@ -62,7 +62,7 @@ class Portfolio extends React.Component {
           <h2><u>FizzBuzz / BingBong</u></h2>
           FizzBuzz? BingBong? It goes by many names in the Developer world, but every iteration holds the same requirement: display a list of numbers which will also output one of these keywords upon the event that a certain operator is met. This is a common JavaScript interview question, and something I liked so much that I decided to do it TWICE!
           <br />
-          <Button href="https://github.com/NannerFox/BingBong" target="_blank" color="primary"><i className="fas fa-code"></i> View Code (1)</Button> <Button href="https://github.com/NannerFox/FizzBuzz" target="_blank" color="primary"><i className="fas fa-code"></i> View Code (2)</Button> <Button href="https://nannerfox.wordpress.com/2018/09/17/fizz-buzz-killington/" target="_blank" color="primary"><i className="fab fa-wordpress-simple"></i> View Blog Post</Button>
+          <Button className="buttonz" href="https://github.com/NannerFox/BingBong" target="_blank" color="primary"><i className="fas fa-code"></i> View Code (1)</Button> <Button className="buttonz" href="https://github.com/NannerFox/FizzBuzz" target="_blank" color="primary"><i className="fas fa-code"></i> View Code (2)</Button> <Button className="buttonz" href="https://nannerfox.wordpress.com/2018/09/17/fizz-buzz-killington/" target="_blank" color="primary"><i className="fab fa-wordpress-simple"></i> View Blog Post</Button>
         </Col>
       </Row>
       <br />
@@ -72,7 +72,7 @@ class Portfolio extends React.Component {
           <h2><u>Ionic-ToDo</u></h2>
           To-Do list for Android Mobile built in Ionic-Cordova, and based on the AngularJS framework.
           <br />
-          <Button href="https://github.com/NannerFox/Ionic-ToDo" target="_blank" color="primary"><i className="fas fa-code"></i> View Code</Button>
+          <Button className="buttonz" href="https://github.com/NannerFox/Ionic-ToDo" target="_blank" color="primary"><i className="fas fa-code"></i> View Code</Button>
         </Col>
       </Row>
       <br />
@@ -82,7 +82,7 @@ class Portfolio extends React.Component {
           <h2><u>Fibonacci-Counter</u></h2>
           Tiny JavaScript project utilizing math skills to output a Fibonacci-esque sequence of numbers. The requirements were to remove the first 2 numbers of the sequence (0, 1), and start the list from there.
           <br />
-          <Button href="https://github.com/NannerFox/Fibonacci-Counter" target="_blank" color="primary"><i className="fas fa-code"></i> View Code</Button>
+          <Button className="buttonz" href="https://github.com/NannerFox/Fibonacci-Counter" target="_blank" color="primary"><i className="fas fa-code"></i> View Code</Button>
         </Col>
       </Row>
       <br />
@@ -92,7 +92,7 @@ class Portfolio extends React.Component {
           <h2><u>Github Profile</u></h2>
           View the rest of my projects and activities on Github!
           <br />
-          <Button href="https://github.com/NannerFox" target="_blank" color="primary"><i className="fab fa-github"></i> Visit Site</Button>
+          <Button className="buttonz" href="https://github.com/NannerFox" target="_blank" color="primary"><i className="fab fa-github"></i> Visit Site</Button>
         </Col>
       </Row>
     </Jumbotron>
