04c0048c34d4106b155a7dd6d55313c8a58fafc5
Unknown <nannerfox@me.com>
2018-11-07 19:37:34 -0600

    Landing Page Alignment

--- src/Landing.js
+++ src/Landing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Fade, Jumbotron } from 'reactstrap';
+import { Fade, Jumbotron, Row, Col } from 'reactstrap';
 
 class Landing extends React.Component {
   constructor(props) {
@@ -19,29 +19,29 @@ class Landing extends React.Component {
       </Jumbotron>
       <Jumbotron className="bg-dark text-center">
         <h3>With Emphasis and Proficiency In:</h3>
-      <a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"><div className="landingIcons"><i className="fab fab fa-html5 fa-2x"></i> HTML</div></a>
+      <a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"><Row className="landingIcons"><Col sm="2"><i className="fab fab fa-html5 fa-2x"></i></Col><Col className="rightSide" sm="10">HTML</Col></Row></a>
       <br />
-      <a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><div className="landingIcons"><i className="fab fa-js-square fa-2x"></i> JavaScript</div></a>
+      <a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><Row className="landingIcons"><Col sm="2"><i className="fab fa-js-square fa-2x"></i></Col><Col className="rightSide" sm="10">JavaScript</Col></Row></a>
       <br />
-      <a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><div className="landingIcons"><i className="fab fa-css3 fa-2x"></i> CSS</div></a>
+      <a rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><Row className="landingIcons"><Col sm="2"><i className="fab fa-css3 fa-2x"></i></Col><Col className="rightSide" sm="10">CSS</Col></Row></a>
       <br />
-      <a rel="noopener noreferrer" href="https://reactjs.org/" target="_blank"><div className="landingIcons"><i className="fab fa-react fa-2x"></i> ReactJS</div></a>
+      <a rel="noopener noreferrer" href="https://reactjs.org/" target="_blank"><Row className="landingIcons"><Col sm="2"><i className="fab fa-react fa-2x"></i></Col><Col className="rightSide" sm="10">ReactJS</Col></Row></a>
       <br />
-      <a rel="noopener noreferrer" href="https://nodejs.org/en/" target="_blank"><div className="landingIcons"><i className="fab fa-node fa-2x"></i> NodeJS</div></a>
+      <a rel="noopener noreferrer" href="https://nodejs.org/en/" target="_blank"><Row className="landingIcons"><Col sm="2"><i className="fab fa-node fa-2x"></i></Col><Col className="rightSide" sm="10">NodeJS</Col></Row></a>
       <br />
-      <a rel="noopener noreferrer" href="https://git-scm.com/" target="_blank"><div className="landingIcons"><i className="fab fa-git-square fa-2x"></i> Git/Version Control</div></a>
+      <a rel="noopener noreferrer" href="https://git-scm.com/" target="_blank"><Row className="landingIcons"><Col sm="2"><i className="fab fa-git-square fa-2x"></i></Col><Col className="rightSide" sm="10">Git/Version Control</Col></Row></a>
       <br />
-      <div className="landingIcons"><i className="fas fa-database fa-2x"></i> SQL/noSQL Databases</div>
+      <Row className="landingIcons"><Col sm="2"><i className="fas fa-database fa-2x"></i></Col><Col className="rightSide" sm="10">SQL/noSQL Databases</Col></Row>
       <br />
-      <a rel="noopener noreferrer" href="https://angularjs.org/" target="_blank"><div className="landingIcons"><i className="fab fa-angular fa-2x"></i> AngularJS</div></a>
+      <a rel="noopener noreferrer" href="https://angularjs.org/" target="_blank"><Row className="landingIcons"><Col sm="2"><i className="fab fa-angular fa-2x"></i></Col><Col className="rightSide" sm="10">AngularJS</Col></Row></a>
       <br />
-      <div className="landingIcons"><i className="fab fa-windows fa-2x"></i> Desktop Applications</div>
+      <Row className="landingIcons"><Col sm="2"><i className="fab fa-windows fa-2x"></i></Col><Col className="rightSide" sm="10">Desktop Applications</Col></Row>
       <br />
-      <a rel="noopener noreferrer" href="https://wordpress.com/" target="_blank"><div className="landingIcons"><i className="fab fa-wordpress fa-2x"></i> Wordpress</div></a>
+      <a rel="noopener noreferrer" href="https://wordpress.com/" target="_blank"><Row className="landingIcons"><Col sm="2"><i className="fab fa-wordpress fa-2x"></i></Col><Col className="rightSide" sm="10">Wordpress</Col></Row></a>
       <br />
-      <div className="landingIcons"><i className="fab fa-aws fa-2x"></i> Deployment Options</div>
+      <Row className="landingIcons"><Col sm="2"><i className="fab fa-aws fa-2x"></i></Col><Col className="rightSide" sm="10">Deployment Options</Col></Row>
       <br />
-      <div className="landingIcons"><i className="fas fa-mobile-alt fa-2x"></i> Mobile & Responsive Design</div>
+      <Row className="landingIcons"><Col sm="2"><i className="fas fa-mobile-alt fa-2x"></i></Col><Col className="rightSide" sm="10">Mobile & Responsive Design</Col></Row>
       <br />
       </Jumbotron>
     </Fade>
