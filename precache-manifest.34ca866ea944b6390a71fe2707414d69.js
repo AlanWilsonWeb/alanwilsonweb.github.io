34c1864e524a276822c31f1ae03af39e4ac824f6
Alan Wilson <nannerfox@me.com>
2019-01-02 19:42:28 -0600

    Updates

---
+++ precache-manifest.34ca866ea944b6390a71fe2707414d69.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "27a1b15a81398fc66a15",
+    "url": "/static/css/main.03d89d43.chunk.css"
+  },
+  {
+    "revision": "27a1b15a81398fc66a15",
+    "url": "/static/js/main.27a1b15a.chunk.js"
+  },
+  {
+    "revision": "e81997bf0df0c977e5ca",
+    "url": "/static/css/1.9ba3f4d1.chunk.css"
+  },
+  {
+    "revision": "e81997bf0df0c977e5ca",
+    "url": "/static/js/1.e81997bf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d17e5249a4733db02a81a7bdee803353",
+    "url": "/static/media/resume.d17e5249.pdf"
+  },
+  {
+    "revision": "55e397c11b2916b45d9bc53ed17b1c16",
+    "url": "/static/media/moviefive.55e397c1.jpg"
+  },
+  {
+    "revision": "f4bf8b300f79f883d95567ce10f45c1c",
+    "url": "/static/media/hashrouter.f4bf8b30.jpg"
+  },
+  {
+    "revision": "5289fa5a8d0e7b6f538d83743a9e8c3b",
+    "url": "/static/media/scriptquiz.5289fa5a.jpg"
+  },
+  {
+    "revision": "2f1e7117cb9a72ab601c9b9847c3a732",
+    "url": "/static/media/bingbong.2f1e7117.jpg"
+  },
+  {
+    "revision": "847118bd47077ebd9dc003a6a212030f",
+    "url": "/static/media/fibonacci.847118bd.jpg"
+  },
+  {
+    "revision": "fc251dde382485783de0ee6a9e907ccb",
+    "url": "/static/media/todolist.fc251dde.jpg"
+  },
+  {
+    "revision": "1423fa7fad8c2c4af2aee53ed0ab2815",
+    "url": "/static/media/brewgoggles.1423fa7f.jpg"
+  },
+  {
+    "revision": "b5e3031014b3cdc76b7f591abe44d946",
+    "url": "/static/media/closingtag.b5e30310.jpg"
+  },
+  {
+    "revision": "ca097fa4b5a86cf5f8d762df25a8e598",
+    "url": "/static/media/stpats.ca097fa4.jpg"
+  },
+  {
+    "revision": "ec59cd39dbd43999f081ab11fb99fdc6",
+    "url": "/static/media/profile.ec59cd39.jpg"
+  },
+  {
+    "revision": "9e5120676c5ab634185191179cc43e12",
+    "url": "/index.html"
+  }
+];
